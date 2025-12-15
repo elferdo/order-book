e7f55322393137ba4121c30f300102cdc6d4217d
Fernando Herrero Carrón <dev@fhcarron.com>
2025-12-15 20:08:29 +0100

    Fix price queries

--- repositories/src/stats.rs
+++ repositories/src/stats.rs
@@ -5,7 +5,7 @@ use crate::Repository;
 
 impl<'c> StatsRepository for Repository<'c> {
     async fn buy_price(&mut self) -> Result<f32, model::stats::repository::StatsRepositoryError> {
-        let row = query!("SELECT MIN(price) AS price FROM bid;")
+        let row = query!("SELECT MAX(price) AS price FROM bid LEFT JOIN match ON match.bid = bid.id WHERE match.ask IS NULL;")
             .fetch_one(&mut *self.conn)
             .await
             .map_err(|_| StatsRepositoryError::DatabaseError)?;
@@ -18,7 +18,7 @@ impl<'c> StatsRepository for Repository<'c> {
     }
 
     async fn sell_price(&mut self) -> Result<f32, StatsRepositoryError> {
-        let row = query!("SELECT MAX(price) AS price FROM ask;")
+        let row = query!("SELECT MIN(price) AS price FROM ask LEFT JOIN match ON match.ask = ask.id WHERE match.bid IS NULL;")
             .fetch_one(&mut *self.conn)
             .await
             .map_err(|_| StatsRepositoryError::DatabaseError)?;
