ce8e52ee30d79228f4c3d43616d3b79f412ecc25
Fernando Herrero Carrón <dev@fhcarron.com>
2025-12-18 10:22:28 +0100

    Async test with sqlx

--- repositories/tests/main.rs
+++ repositories/tests/main.rs
@@ -1,20 +1,30 @@
+use anyhow::Result;
+use rstest::*;
+use sqlx::postgres::PgPoolOptions;
 use testcontainers_modules::{
-    postgres::{self, Postgres},
-    testcontainers::{ContainerRequest, ImageExt, runners::SyncRunner},
+    postgres::Postgres,
+    testcontainers::{ImageExt, runners::AsyncRunner},
 };
+use url::Url;
 
-#[test]
-fn test1() {
+#[rstest]
+#[tokio::test]
+async fn test1_test() -> Result<()> {
     let postgres_instance = Postgres::default()
         .with_init_sql(include_str!("markets.sql").to_string().into_bytes())
         .with_user("fernando")
+        .with_password("postgres")
         .with_tag("17.7")
         .start()
-        .unwrap();
+        .await?;
 
     let connection_string = format!(
-        "postgres://postgres:postgres@{}:{}/postgres",
-        postgres_instance.get_host().unwrap(),
-        postgres_instance.get_host_port_ipv4(5432).unwrap()
+        "postgres://fernando:postgres@{}:{}/postgres",
+        postgres_instance.get_host().await?,
+        postgres_instance.get_host_port_ipv4(5432).await?
     );
+
+    let pool = PgPoolOptions::new().connect(&connection_string).await?;
+
+    Ok(())
 }
