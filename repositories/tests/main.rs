260819fecd3ed4093b64be86fb2f24446a4b9819
Fernando Herrero Carrón <dev@fhcarron.com>
2025-12-19 11:33:41 +0100

    Working tests with real database

--- repositories/tests/main.rs
+++ repositories/tests/main.rs
@@ -1,18 +1,17 @@
 use anyhow::Result;
-use model::order::bid::Bid;
-use model::order::repository::OrderRepository;
+use model::user::repository::UserRepository;
+use model::user::user::User;
 use repositories::Repository;
 use rstest::*;
-use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, postgres::PgPoolOptions};
 use testcontainers_modules::{
     postgres::Postgres,
-    testcontainers::{ImageExt, runners::AsyncRunner},
+    testcontainers::{ContainerAsync, ImageExt, runners::AsyncRunner},
 };
 use uuid::{ContextV7, Timestamp, Uuid};
 
-#[rstest]
-#[tokio::test]
-async fn test1_test() -> Result<()> {
+#[fixture]
+async fn postgres_instance() -> Result<ContainerAsync<Postgres>> {
     let postgres_instance = Postgres::default()
         .with_init_sql(include_str!("markets.sql").to_string().into_bytes())
         .with_user("fernando")
@@ -21,10 +20,20 @@ async fn test1_test() -> Result<()> {
         .start()
         .await?;
 
+    Ok(postgres_instance)
+}
+
+#[rstest]
+#[tokio::test]
+async fn user_with_no_orders(
+    #[future] postgres_instance: Result<ContainerAsync<Postgres>>,
+) -> Result<()> {
+    let db_instance = postgres_instance.await?;
+
     let connection_string = format!(
         "postgres://fernando:postgres@{}:{}/postgres",
-        postgres_instance.get_host().await?,
-        postgres_instance.get_host_port_ipv4(5432).await?
+        db_instance.get_host().await?,
+        db_instance.get_host_port_ipv4(5432).await?
     );
 
     let pool = PgPoolOptions::new().connect(&connection_string).await?;
@@ -35,16 +44,53 @@ async fn test1_test() -> Result<()> {
 
     let context = ContextV7::new();
     let timestamp = Timestamp::now(context);
-    let id = Uuid::new_v7(timestamp);
 
-    let bid = Bid::new(timestamp, id, 1.23);
+    let user = User::new(timestamp);
 
-    repo.persist_bid(&bid).await?;
+    repo.persist_user(&user).await?;
 
-    /*
-        let recover = repo.find_bid(bid.get_id()).await?;
+    let recover = repo
+        .find_user(model::lock_mode::LockMode::KeyShare, user.get_id())
+        .await?;
 
-        assert_eq!(bid.get_id(), recover.get_id());
-    */
+    assert_eq!(user.get_id(), recover.get_id());
+
+    Ok(())
+}
+
+#[rstest]
+#[tokio::test]
+async fn user_with_ask(
+    #[future] postgres_instance: Result<ContainerAsync<Postgres>>,
+) -> Result<()> {
+    let db_instance = postgres_instance.await?;
+
+    let connection_string = format!(
+        "postgres://fernando:postgres@{}:{}/postgres",
+        db_instance.get_host().await?,
+        db_instance.get_host_port_ipv4(5432).await?
+    );
+
+    let pool = PgPoolOptions::new().connect(&connection_string).await?;
+
+    let mut conn = pool.acquire().await?;
+
+    let mut repo = Repository::new(&mut conn).await;
+
+    let context = ContextV7::new();
+    let timestamp = Timestamp::now(context);
+
+    let mut user = User::new(timestamp);
+
+    let _ = user.ask(timestamp, 4.32);
+
+    repo.persist_user(&user).await?;
+
+    let recover = repo
+        .find_user(model::lock_mode::LockMode::KeyShare, user.get_id())
+        .await?;
+
+    assert_eq!(user.get_id(), recover.get_id());
+
     Ok(())
 }
