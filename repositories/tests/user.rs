c4f53eef6dc9e244c96ba848aa12d3621d1fc356
Fernando Herrero Carrón <dev@fhcarron.com>
2025-12-19 19:12:27 +0100

    New tests

--- repositories/tests/user.rs
+++ repositories/tests/user.rs
@@ -1,13 +1,13 @@
 use anyhow::Result;
+use model::repository_error::RepositoryError;
 use model::user::user::User;
 use model::{lock_mode::LockMode, user::repository::UserRepository};
 use repositories::Repository;
-use rstest::*;
-use sqlx::{PgPool, Pool, postgres::PgPoolOptions, query};
+use sqlx::{PgPool, query};
 use uuid::{ContextV7, Timestamp, Uuid};
 
 #[sqlx::test]
-async fn user_with_no_orders(pool: PgPool) -> Result<()> {
+async fn persist_user(pool: PgPool) -> Result<()> {
     let mut conn = pool.acquire().await?;
 
     let mut repo = Repository::new(&mut conn).await;
@@ -19,17 +19,17 @@ async fn user_with_no_orders(pool: PgPool) -> Result<()> {
 
     repo.persist_user(&user).await?;
 
-    let recover = repo
-        .find_user(model::lock_mode::LockMode::KeyShare, user.get_id())
+    let recover = query!("SELECT * FROM public.user WHERE id = $1", user.get_id())
+        .fetch_one(&mut *conn)
         .await?;
 
-    assert_eq!(user.get_id(), recover.get_id());
+    assert_eq!(*user.get_id(), recover.id);
 
     Ok(())
 }
 
 #[sqlx::test]
-async fn user_with_ask(pool: PgPool) -> Result<()> {
+async fn persist_user_with_ask(pool: PgPool) -> Result<()> {
     let mut conn = pool.acquire().await?;
 
     let mut repo = Repository::new(&mut conn).await;
@@ -43,17 +43,20 @@ async fn user_with_ask(pool: PgPool) -> Result<()> {
 
     repo.persist_user(&user).await?;
 
-    let recover = repo
-        .find_user(model::lock_mode::LockMode::KeyShare, user.get_id())
-        .await?;
+    let recover = query!(
+        "SELECT * FROM ask WHERE user = $1",
+        user.get_id().to_string()
+    )
+    .fetch_one(&mut *conn)
+    .await?;
 
-    assert_eq!(user.get_id(), recover.get_id());
+    assert_eq!(4.32, recover.price);
 
     Ok(())
 }
 
 #[sqlx::test(fixtures("first_user"))]
-async fn sqlx_test(pool: PgPool) -> Result<()> {
+async fn find_user_when_id_exists(pool: PgPool) -> Result<()> {
     let mut a = pool.acquire().await?;
 
     let mut repo = Repository::new(&mut a).await;
@@ -68,16 +71,16 @@ async fn sqlx_test(pool: PgPool) -> Result<()> {
 }
 
 #[sqlx::test(fixtures("first_user", "asks"))]
-async fn sqlx_test2(pool: PgPool) -> Result<()> {
+async fn find_user_when_id_does_not_exist(pool: PgPool) -> Result<()> {
     let mut a = pool.acquire().await?;
 
     let mut repo = Repository::new(&mut a).await;
 
-    let id = Uuid::parse_str("019b36f8-bb74-7ad3-8a02-465301b72d92")?;
+    let id = Uuid::parse_str("019b37bd-e9ef-742a-995a-d49255ce41f3")?;
 
-    let user = repo.find_user(LockMode::None, &id).await?;
+    let user = repo.find_user(LockMode::None, &id).await;
 
-    assert_eq!(*user.get_id(), id);
+    assert!(matches!(user, Err(RepositoryError::DatabaseError(_))));
 
     Ok(())
 }
