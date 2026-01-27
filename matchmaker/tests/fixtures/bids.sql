8ed301785e0b15fbfbe6f6572004d970c99e8372
Fernando Herrero Carrón <dev@fhcarron.com>
2026-01-27 03:57:02 +0100

    Add sqlx tests to matchmaker

---
+++ matchmaker/tests/fixtures/bids.sql
@@ -0,0 +1,5 @@
+INSERT INTO bid (id, "user", price) VALUES 
+ ('019b3789-d0cb-7af7-8c72-e103d019c4ec', '019b3788-2df4-70a9-b38b-fd5d79be3473', 4.56),
+ ('019b3789-d0cd-74e7-b0a2-cc9fa07d1f0a', '019b3788-2df7-7b33-bdfe-eabb1b67b5fc', 5.67),
+ ('019b3789-d0d0-78c8-9018-532a19d30a52', '019b3788-2dfa-74aa-a8f4-39978972c2a3', 6.78)
+;
---
+++ matchmaker/tests/fixtures/first_user.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.user VALUES 
+ ('019b3788-2ded-7f19-8191-9018a3939f60'),
+ ('019b36f8-bb74-7ad3-8a02-465301b72d92'),
+ ('019b3788-2df1-7ccd-b08c-fefc19b864d6'),
+ ('019b3788-2df4-70a9-b38b-fd5d79be3473'),
+ ('019b3788-2df7-7b33-bdfe-eabb1b67b5fc'),
+ ('019b3788-2dfa-74aa-a8f4-39978972c2a3')
+;
